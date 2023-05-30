a522d6434d9b933da6b7ce94e928d22f9cf4edc2
Eddy <edkosyan2000@gmail.com>
2023-05-30 16:12:56 +0400

    initial commit

---
+++ src/pages/Chat.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const Chat = React.memo(() => {
+    return <>Chat</>
+})
+
+export default Chat
---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SignIn = React.memo(() => {
+    return <>SignIn</>
+})
+
+export default SignIn
