a4b0d746ee1f304c79b0015e2e23625e7388420f
Eddy <edkosyan2000@gmail.com>
2023-06-01 15:49:47 +0400

    use hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,17 @@ import App from "./App.tsx";
 import "./styles/reset.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "./store/store.ts";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/whats-app/">
+    <HashRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
