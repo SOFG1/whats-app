6599ce26fef07087ecc495f588de48f2bc8c49ac
Eddy <edkosyan2000@gmail.com>
2023-06-01 15:44:10 +0400

    add base name

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/whats-app/">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
